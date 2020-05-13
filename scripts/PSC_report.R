6d48e8693940d2e6e050c890e0cc349c51ac199e
Louth-Robins <lou049@csiro.au>
2020-05-13 11:45:41 +0930

    Incorporated read_xl function to read from original xlsm spreadsheets, identify sheets and clean up columns/headers accordingly. Because part of the tidy was handled at this stage, code for some of the df tidy was pared back.

--- scripts/PSC_report.R
+++ scripts/PSC_report.R
@@ -3,49 +3,69 @@ library(readxl)
 library(purrr)
 library(lubridate)
 
-remotes::install_github("sparce/DSreport") # installing Project Template.
-
 # Cache CSV snapshot using readxl of raw datatables from PSC_JIRA and PSC_O2D report 
-# workbooks. Using read_xl currently results in an error.
+# workbooks. 
 
-# PSC_JIRA_xl <- readxl_example(path = 'data/PSC_JIRA_Mar20.xlsm') %>% 
-#    read_excel(sheet = "Created") %>% 
-#    write_csv('data/JIRA_Mar20_created.csv')
+# O2D REPORT READ AND CONVERSION TO CSV
+
+O2D_assigned <- read_excel("data/PSC_O2D_Mar20.xlsm",  #Q: possible to find/replace filenames, i.e. latest version?
+                          sheet = "Assigned", 
+                          col_names = TRUE) %>%  
+  write_csv("data/O2D_assigned.csv")
+
+O2D_completed <- read_excel("data/PSC_O2D_Mar20.xlsm",
+                           sheet = "Completed", 
+                           col_names = TRUE) %>%  
+  write_csv("data/O2D_completed.csv")
+
+O2D_prev_month <- read_excel("data/PSC_O2D_Mar20.xlsm",
+                             sheet = "Open from previous mths") %>% 
+  write_csv("data/O2D_prev_mths.csv")
+
+
+# JIRA REPORT READ AND CONVERSION TO CSV
+# note: requires additional code to deal with absence of conventional Headers in spreadsheet
+
+JIRA_assigned <- read_excel("data/PSC_JIRA_Mar20.xlsm", 
+                      sheet = "Created",
+                      col_names = c("class", "issue", "key_ident", "issue_id", "summary", "status", "resolution", "created", "resolved", "reporter", "assignee", "misc"),
+                      col_type = c("text", "text", "numeric", "numeric", "text", "text", "text", "date", "date", "text", "text", "text")) %>% 
+  write_csv("data/JIRA_assigned.csv")
+
+JIRA_completed <- read_excel("data/PSC_JIRA_Mar20.xlsm", 
+                            sheet = "Completed",
+                            col_names = c("class", "issue", "key_ident", "issue_id", "summary", "status", "resolution", "created", "resolved", "reporter", "assignee", "misc", "misc"),
+                            col_type = c("text", "text", "numeric", "numeric", "text", "text", "text", "date", "date", "text", "text", "text", "text")) %>% 
+  write_csv("data/JIRA_completed.csv")
+
 
 ## Include additional code to dispose sheets into .csv files with appropriate naming conventions.
 
 # IMPORT CSV EXPORTS AS NEW VARIABLES
 
-# JIRA REPORTS
-JIRA_open <- read_csv('data/PSC_JIRA_Mar20_created.csv')              #Q: possible to find/replace filenames, i.e. latest version?
-JIRA_completed <- read_csv('data/PSC_JIRA_Mar20_completed.csv')
+# O2D REPORT DATAFRAMES
+O2D_assigned <- read_csv('data/O2D_assigned.csv')
+O2D_completed <- read_csv('data/O2D_completed.csv')
+O2D_prev_mths <- read_csv('data/O2D_prev_mths.csv')
 
-# O2D REPORTS
-O2D_assigned <- read_csv('data/PSC_O2D_Mar20_assigned.csv')
-O2D_completed <- read_csv('data/PSC_O2D_Mar20_completed.csv')
-O2D_open_prev_month <- read_csv('data/PSC_O2D_Mar20_prev_month.csv')
+# JIRA REPORT DATAFRAMES
+JIRA_open <- read_csv('data/JIRA_assigned.csv')             
+JIRA_completed <- read_csv('data/JIRA_completed.csv')
+
+
+JIRA_open
+JIRA_completed
 
 # JIRA & O2D DATA TIDY & TRANSFORM
-# The JIRA reports (from xlsm files) originally has blank headers (X1, X2..) and the text formatting 
-# had defaulted to 'chr'. This is why the rename function in called, with slice used to remove blank
-# rows below the blank headers. At a later stage, it would be good to reformat the text format of given
-# columns - i.e. issue_ident to dbl, created and resolved to dttm. Not necessary for the purposes of this 
-# code but might be handy in future if data is to be manipulated in other ways.
 
 JIRA_open <- 
-  slice(JIRA_open, -0:-3, preserve = FALSE) %>% 
-  select(1:11) %>% 
-  rename(class = X1, issue_type = X2, Key = X3, issue_ident = X4, 
-       summary = X5, status = X6, resolution = X7, created = X8, resolved = X9, 
-       reporter = X10, assignee = X11) %>% 
+  slice(JIRA_open, -1:-3, preserve = FALSE) %>% 
+  select(-3) %>% 
   na.omit() # remove all rows containing 'NA'
 
 JIRA_completed <- 
-  slice(JIRA_completed, -0:-3, preserve = FALSE) %>% 
-  select(1:11) %>% 
-  rename(class = X1, issue_type = X2, Key = X3, issue_ident = X4, 
-         summary = X5, status = X6, resolution = X7, created = X8, resolved = X9, 
-         reporter = X10, assignee = X11)  %>% 
+  slice(JIRA_completed, -1:-3, preserve = FALSE) %>% 
+  select(-3, -10:-13) %>% 
   na.omit() # remove all rows containing 'NA'
 
 JIRA_open_class <- group_by(JIRA_open, class) %>% 
@@ -59,18 +79,18 @@ JIRA_completed_class
 
 ## O2D DATA TIDY & TRANSFORM
 
-O2D_assigned <- 
+O2D_assigned_class <- 
   rename(O2D_assigned, class = `Task Type`) %>% 
   group_by(class) %>% 
   summarise(count = n())
 
-O2D_completed <- 
+O2D_completed_class <- 
   rename(O2D_completed, class = `Task Type`) %>% 
   group_by(class) %>% 
   summarise(count = n())
 
-O2D_open_prev_month <- 
-  rename(O2D_open_prev_month, class = `Task Type`) %>% 
+O2D_prev_mths_class <- 
+  rename(O2D_prev_mths, class = `Task Type`) %>% 
   group_by(class) %>% 
   summarise(count = n())
 
